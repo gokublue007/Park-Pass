f085bc6ca9c5f434ac6467ade0f37e66700ff8ed
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-03-31 21:21:13 -0600

    no idea

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 console.log("Park Dev Rangers");
 var parkCodes = [];
 var parkNames = [];
-var parkDictionary = [];
 var apiKey = 'TNFSbiKup0DvQBzqjbD6tCiZTq4j6SBhWGF4hCOQ';
 
+// Pulls park names and codes to populate drop down and reference in future API pulls
 function getParkCodes() {
   var parkPullURL = `https://developer.nps.gov/api/v1/parks?limit=500&api_key=${apiKey}`;
 
