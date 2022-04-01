3ec603228e9b34f351d0a4a7cf6dfe9bede865eb
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-03-31 19:57:17 -0600

    started api pull function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log("Park Dev Rangers");
+
+function getParkData() {
+  // Create variables for both current weather & five day Open Weather Map API's
+  var apiKey = 'TNFSbiKup0DvQBzqjbD6tCiZTq4j6SBhWGF4hCOQ';
+  var parkCode = 'acad';
+  var parkPullURL = `https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=${apiKey}`;
+
+  // Pull data and display today's weather
+  $.ajax({
+    url: parkPullURL,
+    method: 'GET',
+  }).then(function (response) {
+    console.log(response);
+  })
+}
+
+getParkData();
