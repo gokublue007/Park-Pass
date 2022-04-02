b7a57f9bb40b58363322d7cc9b0eab46b8edc1b3
Peter Vrutneski <pvrutneski@gmail.com>
2022-04-02 14:12:51 -0600

    added function to pull park name to local storage

--- Assest/js/script.js
+++ Assest/js/script.js
@@ -1,12 +1,12 @@
-var inputTextBox = $('.input');
+var inputTextBox = $(".input");
 var searchParkButtonEl = $("#searchParkButton");
 var parkCodeList = [];
 var parkNameList = [];
-var NPSApiKey = 'TNFSbiKup0DvQBzqjbD6tCiZTq4j6SBhWGF4hCOQ';
-var googleApiKey = 'AIzaSyA_Szh6txcLm9SOSbuZV-CyqKVbqljMkTM'
+var NPSApiKey = "TNFSbiKup0DvQBzqjbD6tCiZTq4j6SBhWGF4hCOQ";
+var googleApiKey = "AIzaSyA_Szh6txcLm9SOSbuZV-CyqKVbqljMkTM";
 var inputText = "rocky mountain national park";
-var hikingContent = $('#hiking-content');
-var parkInfoContent = $('#park-info-content');
+var hikingContent = $("#hiking-content");
+var parkInfoContent = $("#park-info-content");
 var parkLatitude;
 var parkLongitude;
 var parkCode;
@@ -15,11 +15,10 @@ var parkEntryFee;
 var parkImageLink;
 var parkHomepageLink;
 
-
 // Place holder for input text until we get that functionally working
 // var inputText = "Abraham Lincoln Birthplace National Historical Park";
 
-// This function will take the input the user types into the search box, find the spot in the parkNamees array that matches the input and return the corresponding park code from the 
+// This function will take the input the user types into the search box, find the spot in the parkNamees array that matches the input and return the corresponding park code from the
 // parkCodes array
 function findParkCode(chosenPark) {
   // Create variable that has park code pulled from array based on selection from dropdown
@@ -27,9 +26,21 @@ function findParkCode(chosenPark) {
     if (chosenPark == parkNameList[i]) {
       parkCode = parkCodeList[i];
       // console.log(parkCode);
-      break
+      break;
     }
   }
+}
+
+function saveParkName(parkName) {
+  var storedSavedParkNames = localStorage.getItem("savedParkNames");
+  console.log("storedSavedParkNames: ", storedSavedParkNames);
+  var savedParkNames = [];
+  if (storedSavedParkNames) {
+    savedParkNames = JSON.parse(storedSavedParkNames);
+  }
+  console.log("Saved Park Names: ", savedParkNames);
+  savedParkNames = savedParkNames.concat(parkName);
+  localStorage.setItem("savedParkNames", JSON.stringify(savedParkNames));
 }
 
 // This function will be executed upon clicking the search button
@@ -38,7 +49,7 @@ function pullParkData() {
   var parkPullURL = `https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=${NPSApiKey}`;
   $.ajax({
     url: parkPullURL,
-    method: 'GET',
+    method: "GET",
   }).then(function (response) {
     console.log(response);
     // Set entryFee variable to cost of entry if > 0, to text "Free Fee Park" if there is no entry fee
@@ -76,7 +87,9 @@ function pullParkData() {
     feeDisplayElement.html("Entrance Fee: " + parkEntryFee);
     parkInfoContent.append(feeDisplayElement);
 
-  })
+    var parkFullName = response.data[0].fullName;
+    saveParkName(parkFullName);
+  });
 }
 
 // Function pulls data from NPS API and stores park names and codes to be used later
@@ -84,27 +97,30 @@ function getParkNamesCodes() {
   var allParksURL = `https://developer.nps.gov/api/v1/parks?limit=500&api_key=${NPSApiKey}`;
   $.ajax({
     url: allParksURL,
-    method: 'GET',
+    method: "GET",
   }).then(function (response) {
     // console.log(response);
     for (i = 0; i < response.data.length; i++) {
       parkCodeList.push(response.data[i].parkCode);
       parkNameList.push(response.data[i].fullName);
     }
-  })
+  });
   console.log(parkNameList);
   // console.log(parkCodeList);
 }
 
 function hikingTrails() {
-  fetch(`https://mighty-headland-78923.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=hiking+trails+${inputText}&key=${googleApiKey}`, {
-    method: 'GET',
-  })
+  fetch(
+    `https://mighty-headland-78923.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=hiking+trails+${inputText}&key=${googleApiKey}`,
+    {
+      method: "GET",
+    }
+  )
     .then(function (response) {
       return response.json();
     })
     .then(function (trailData) {
-      console.log(trailData)
+      console.log(trailData);
     });
 }
 
@@ -119,10 +135,10 @@ function runParkSearch(event) {
   pullParkData();
 }
 
-searchParkButtonEl.on("click", runParkSearch)
+searchParkButtonEl.on("click", runParkSearch);
 
-$( function() {
-    $( "#tags" ).autocomplete({
-    source: parkNameList
+$(function () {
+  $("#tags").autocomplete({
+    source: parkNameList,
   });
-} );
+});
